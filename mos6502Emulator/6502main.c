00f26804f3c08c620c3338fb494e6cf30cb1443f
henriqueZapellaRocha <henri_zap@hotmail.com>
2024-07-15 20:44:17 -0300

    more instructions are added

--- mos6502Emulator/6502main.c
+++ mos6502Emulator/6502main.c
@@ -6,6 +6,9 @@
 
 //instruction opdcode
 const Byte InsLDAIM = 0xA9;
+const Byte InsLDAZP = 0xA5;
+const Byte InsLDAZPX = 0xB5;
+const Byte InsJSRABS = 0X20;
 
 const u32 maxMemorySize = 1024 * 64;
 
@@ -33,7 +36,9 @@ typedef struct CPU {
     
     void (*reset)(struct CPU *cpu, struct Memory *memory);
     void (*executeI)(struct CPU *cpu, struct Memory *memory, u32 cycles);
-    Byte (*fetchInstrucstion)(struct CPU *cpu, struct Memory *memory, u32 cycles);
+    Byte (*fetchInstrucstion)(struct CPU *cpu, struct Memory *memory, u32 *cycles);
+    Byte (*readByteInMemory)(struct Memory *Memory, u32 *cycles, u32 adress);
+    Word (*fetchWord)(struct CPU *cpu, struct Memory *memory, u32 *cycles);
 } CPU;
 
 void reset(CPU *cpu, Memory *memory) {
@@ -50,35 +55,81 @@ void initMemory(Memory *memory) {
     }
 }
 
-Byte fetchInstrucstion(CPU *cpu, Memory *memory, u32 cycles) {
+void writeWord(Word data, u32 address, Memory *memory, u32 *cycles) {
+        memory->Data[address] = data & 0xFF;
+        memory->Data[address+1] = (data >> 8);
+}
+
+Byte readByteInMemory(Memory *memory, u32 *cycles, u32 adrress) {
+    Byte Data = memory->Data[adrress];
+    (*cycles)--;
+    return Data;
+}
+
+Word fetchWord(CPU *cpu, Memory *memory, u32 *cycles) {
+    //take word
+    Word Data = memory->Data[cpu->PC];
+    cpu->PC++;
+    
+    //handle little endian
+    Data |= (memory->Data[cpu->PC] << 8);
+    cpu->PC++;
+    (*cycles)-=2;
+    return Data;
+}
+
+Byte fetchInstrucstion(CPU *cpu, Memory *memory, u32 *cycles) {
     Byte Data = memory->Data[cpu->PC];
     cpu->PC++;
-    cycles--;
+    (*cycles)--;
     return Data;
+}
+
+void updateFlagsLDA(CPU *cpu) {
+    if(cpu->ACC==0) {
+        cpu->Z = 0x1;
+    }
+    if((cpu->ACC & 0b10000000) > 0) {
+        cpu->N = 0x1;
+    }
 }
 
 void executeI(CPU *cpu, Memory *memory, u32 cycles) {
     while(cycles > 0) {
-        Byte opcode = fetchInstrucstion(cpu, memory, cycles);
+        Byte opcode = fetchInstrucstion(cpu, memory, &cycles);
         Byte temp;
         switch (opcode)
         {
         case InsLDAIM: {
-            temp = fetchInstrucstion(cpu, memory, cycles);
+            temp = fetchInstrucstion(cpu, memory,&cycles);
             cpu->ACC = temp;
-            if(temp==0) {
-                cpu->Z = 0x1;
-            }
-            if((temp & 0b10000000) > 0) {
-                cpu->N = 0x1;
-            }
-            cycles--;
+            updateFlagsLDA(cpu);
             break;
+        }
+        case InsLDAZP: {
+            Byte adress = fetchInstrucstion(cpu,memory,&cycles);
+            temp = readByteInMemory(memory, &cycles, adress);
+            cpu->ACC = temp;
+            updateFlagsLDA(cpu);
+        }
+        case InsLDAZPX: {
+            Byte adress = fetchInstrucstion(cpu,memory,&cycles);
+            temp = readByteInMemory(memory, &cycles, adress);
+            temp += cpu->X;
+            cpu->ACC = temp;
+            updateFlagsLDA(cpu);
+        }
+        case InsJSRABS: {
+            Word subAddr = fetchWord(cpu,memory,&cycles);
+            writeWord(cpu->PC-1, cpu->SP,memory,&cycles);
+            memory->Data[cpu->SP] = cpu->PC - 1; 
+            cycles--;
+            cpu->PC = subAddr;
+            cycles--;
         }
         default:
             break;
         }
-        cycles--;
     }
 }
 
@@ -91,22 +142,26 @@ int main() {
     cpu.reset = reset;
     cpu.executeI = executeI;
     cpu.fetchInstrucstion = fetchInstrucstion;
+    cpu.readByteInMemory = readByteInMemory;
+    cpu.fetchWord = fetchWord;
     memory.initMemory = initMemory;
     
     //reset cpu and memory
     cpu.reset(&cpu, &memory);
-
-        // Exemplo: inserir instrução LDA imediato e valor imediato na memória
-    memory.Data[0xFFC] = InsLDAIM; // LDA imediato opcode
-    memory.Data[0XFFD] = 10; // Valor imediato para LDA
+    cpu.X = 10;
+    // Exemplo: inserir instrução LDA imediato e valor imediato na memória
+    memory.Data[0xFFC] = InsLDAZPX; // LDA imediato opcode
+    memory.Data[0XFFD] = 0x42; // Valor imediato para LDA
+    memory.Data[0X42] = 10;
     //testing lda immediate
-    cpu.executeI(&cpu,&memory,2);
+    cpu.executeI(&cpu,&memory,3);
 
     
     
     //verify the values
     printf("CPU PC: %04X\n", cpu.PC);
     printf("CPU SP: %02X\n", cpu.SP);
+    printf("CPU ACC: %0X\n", cpu.ACC);
     printf("Memory[0]: %02X\n", memory.Data[0]);
     
     return 0;
